9331fd5023e98a5b7a369715c403e9d1c6970e73
Roman Shcherba <warboyrom@gmail.com>
2024-06-12 15:37:29 +0300

    Add project files.

---
+++ PortSpeedTestTemplate/Test.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortSpeedTestTemplate
+{
+    public class Test
+    {
+        public static readonly ILog log = LogManager.GetLogger(typeof(Test));
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
