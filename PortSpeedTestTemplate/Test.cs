f0aee5acba6042e57e766bb652a01f83e028cb48
Roman Shcherba <warboyrom@gmail.com>
2024-06-13 11:12:21 +0300

    Added Flow, Test, TestTemplate classes

--- PortSpeedTestTemplate/Test.cs
+++ PortSpeedTestTemplate/Test.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,19 @@ namespace PortSpeedTestTemplate
     public class Test
     {
         public static readonly ILog log = LogManager.GetLogger(typeof(Test));
-        public static void Main(string[] args)
-        {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TestTemplate? Parameters { get; set; }
 
+
+        public Test(string testName, string description, TestTemplate parameter)
+        {
+            Name = testName;
+            Description = description;
+            Parameters = parameter;
         }
+
+        public string TestName { get; }
+
     }
 }
